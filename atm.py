1de8e275028083ea5a033f064377b2ee7cf99e42
rjowell20007 <44178512+rjowell@users.noreply.github.com>
2022-03-10 13:32:00 +0000

    added functions.

--- atm.py
+++ atm.py
@@ -1,1 +1,10 @@
 classNames = ["ashleigh","jayden","jj","nathan","cole"]
+
+'''
+Load Account
+Create a global variable called "currentAccount", set it to 'None'
+ask the user to input a Name
+loop through all accounts in your account list. If one matches the name input, set it to currentACcount
+if there are no matches, say "that account doens't exist."
+
+'''
